dc8fe11e2afca3d830278cdd303db0f963274d1b
RHamudi <ramonramos.silva16@gmail.com>
2024-07-30 21:57:23 -0300

    feat: modificando testes

--- Promocoes.Tests/Tests/JwtTests.cs
+++ Promocoes.Tests/Tests/JwtTests.cs
@@ -4,7 +4,7 @@ using Promocoes.Application.Input.Services.Jwt;
 
 namespace Promocoes.Tests.Tests
 {
-    /*
+    
     [TestClass]
     public class JwtTests
     {
@@ -12,8 +12,10 @@ namespace Promocoes.Tests.Tests
         [TestMethod]
         public void Criar_Token_Jwt()
         {
-            
-            var model = new AuthenticationCommand("123456", "ramonramos.silva19@gmail.com");
+
+            var model = new AuthenticationCommand();
+            model.Email = "ramonramos.silva19@gmail.com";
+            model.Password = "123456";
             var jwToken = new CreateToken(model);
             Assert.IsNotNull(jwToken.Token);
         }
@@ -21,11 +23,13 @@ namespace Promocoes.Tests.Tests
         [TestMethod]
         public void Verificar_Jwt_valido()
         {
-            var model = new AuthenticationCommand("123456", "ramonramos.silva19@gmail.com");
+            var model = new AuthenticationCommand();
+            model.Email = "ramonramos.silva19@gmail.com";
+            model.Password = "123456";
             var jwToken = new CreateToken(model);
             var validateToken = ValidateToken.VerifyToken(jwToken.Token);
 
             Assert.AreEqual(true, validateToken);
         }
-    } */
+    } 
 }
