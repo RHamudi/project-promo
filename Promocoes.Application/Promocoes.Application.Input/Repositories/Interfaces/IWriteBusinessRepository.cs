76bf1425570e8f7cfecb7d51efba925b495d28e6
RHamudi <ramonramos.silva16@gmail.com>
2024-07-30 21:58:37 -0300

    feat: create new method in interface

--- Promocoes.Application/Promocoes.Application.Input/Repositories/Interfaces/IWriteBusinessRepository.cs
+++ Promocoes.Application/Promocoes.Application.Input/Repositories/Interfaces/IWriteBusinessRepository.cs
@@ -1,3 +1,4 @@
+using Promocoes.Application.Input.Commands.BusinessContext;
 using Promocoes.Domain.Entities;
 
 namespace Promocoes.Application.Input.Repositories.Interfaces
@@ -5,5 +6,6 @@ namespace Promocoes.Application.Input.Repositories.Interfaces
     public interface IWriteBusinessRepository
     {
         void InsertBusiness(BusinessEntity business);
+        void UpdateUserByBusiness(string idUser, string idBusiness);
     }
 }
