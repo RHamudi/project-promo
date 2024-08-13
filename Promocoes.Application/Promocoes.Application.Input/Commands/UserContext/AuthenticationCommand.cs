9140561e50b63ebd6c7e27309c0878069aa43d54
RHamudi <ramonramos.silva16@gmail.com>
2024-08-13 10:14:22 -0300

    feat: adding new error verification in authentication

--- Promocoes.Application/Promocoes.Application.Input/Commands/UserContext/AuthenticationCommand.cs
+++ Promocoes.Application/Promocoes.Application.Input/Commands/UserContext/AuthenticationCommand.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
+using OneOf;
+using Promocoes.Application.Input.Commands.UserContext;
 using Promocoes.Application.Input.Receivers;
+using Promocoes.Errors;
 
 namespace Promocoes.Application.Input.Commands.BusinessContext
 {
-    public class AuthenticationCommand : IRequest<State>
+    public class AuthenticationCommand : IRequest<OneOf<AuthenticationDTO, AppError>>
     {
         public string Password { get; set; }
         public string Email { get; set; }
--- Promocoes.Application/Promocoes.Application.Input/Repositories/Interfaces/IAuthenticationRepository.cs
+++ Promocoes.Application/Promocoes.Application.Input/Repositories/Interfaces/IAuthenticationRepository.cs
@@ -1,10 +1,12 @@
+using OneOf;
 using Promocoes.Application.Input.Commands.BusinessContext;
 using Promocoes.Application.Input.Commands.UserContext;
+using Promocoes.Errors;
 
 namespace Promocoes.Application.Input.Repositories.Interfaces
 {
     public interface IAuthenticationRepository
     {
-        AuthenticationDTO Authentication(AuthenticationCommand command);
+        OneOf<AuthenticationDTO, AppError> Authentication(AuthenticationCommand command);
     }
 }
