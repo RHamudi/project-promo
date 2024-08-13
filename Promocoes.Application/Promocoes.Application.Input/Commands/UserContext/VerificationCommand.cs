2124ed98cf29c53d4796cd74f823b54b6b01a94d
RHamudi <ramonramos.silva16@gmail.com>
2024-08-13 10:19:19 -0300

    feat: adding new type of mediatr

--- Promocoes.Application/Promocoes.Application.Input/Commands/UserContext/VerificationCommand.cs
+++ Promocoes.Application/Promocoes.Application.Input/Commands/UserContext/VerificationCommand.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
+using OneOf;
 using Promocoes.Application.Input.Receivers;
+using Promocoes.Errors;
 
 namespace Promocoes.Application.Input.Commands.UserContext
 {
-    public class VerificationCommand : IRequest<State>
+    public class VerificationCommand : IRequest<OneOf<VerificationCommand, AppError>>
     {
         public string Token { get; set; }
     }
